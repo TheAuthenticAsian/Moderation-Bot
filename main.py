96508587cb51574aadf0e398044411b9272d578e
TheAuthenticAsian <prasen1265@gmail.com>
2021-06-07 10:42:04 -0700

    made the load_cog_files method run recursively so we can add cogs into different folders now

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import time
+
+start_time = time.time()
 
 # enviroment thing
 load_dotenv()
@@ -27,14 +30,39 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"{bot_prefix}help"))
 
 # load cogs
-for file in os.listdir(cogs_folder):
-    if file.endswith(".py"):
-        try:
-            client.load_extension(f"cogs.{file[:-3]}")
-            print(f"Loaded: {file}")
-        except Exception as e:
-            print(f"Could not load: {file}")
-            print(e)
 
+
+def load_cog_files(folder: str):
+    """[Load Cogs Recursively]
+
+    Args:
+        folder (str): [Folder Path]
+    """
+
+    # We don't want to load pycache folder if there is one
+    if folder.find("__pycache__") != -1:
+        return
+
+    extension_name = folder.replace("/", ".")[2:]
+
+    for file in os.listdir(folder):
+        # delete the first two characters because they're ".." because of the replace method
+
+        if file.endswith(".py"):
+            try:
+                client.load_extension(f"{extension_name}.{file[:-3]}")
+                print(f"Loaded: {file}")
+            except Exception as e:
+                print(f"Could not load: {file}")
+                print(e)
+            continue
+
+        if os.path.isdir(f"{folder}/{file}"):
+            load_cog_files(f"{folder}/{file}")
+
+
+load_cog_files(cogs_folder)
+
+print("My program took", time.time() - start_time, "to run")
 
 client.run(bot_token)
